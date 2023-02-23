aba717506689ee1b42b56aab255841bb7eb7f54c
运辉 郑 <631697250@qq.com>
2023-02-23 10:40:28 +0800

    add Guid

---
+++ Engine/Source/Core/Misc/Guid.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <istream>
+#include <sstream>
+#include "Core/Config.h"
+
+
+namespace Eggy
+{
+	class Guid
+	{
+		typedef boost::uuids::uuid UUID;
+	public:
+		Guid() = default;
+		Guid(const Guid& rhs) noexcept = default;
+		Guid(Guid&& rhs) noexcept = default;
+		explicit Guid(const String& str) noexcept
+		{
+			*this = FromString(str);
+		}
+
+		static constexpr uint32 GetStringSize() { return sizeof(uint8) * 32 + sizeof(char) * 3; }
+		
+		static Guid Generate()
+		{
+			UUID uid = boost::uuids::random_generator()();
+			return *(Guid*)&uid;
+		}
+		
+		static Guid FromString(const String& s)
+		{
+			HYBRID_CHECK(s.size() == GetStringSize());
+			const char* rule = "XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX";
+			Guid guid{};
+			std::stringstream ss;
+			for (size_t i = 0; i < s.size(); ++i)
+			{
+				if (rule[i] == 'X')
+				{
+					HYBRID_CHECK(isxdigit(s[i]));
+					ss << s[i];
+				}
+				else
+				{
+					HYBRID_CHECK(s[i] == '-');
+					ss >> std::hex >> guid.mU32[i / 8];
+					ss.clear();
+				}
+			}
+			return guid;
+		}
+
+		String ToString() const noexcept
+		{
+			std::stringstream ss;
+			for (int i = 0; i < 4; ++i)
+			{
+				ss << std::setfill('0') << std::setw(8) << std::hex << mU32[i];
+				if (i != 3)
+					ss << '-';
+			}
+			return String(ss.str());
+		}
+
+		Guid& operator = (const Guid& rhs) noexcept = default;
+		Guid& operator = (Guid&& rhs) noexcept = default;
+		inline bool operator == (Guid& rhs) { return memcmp(mU32, rhs.mU32, sizeof(uint32) * 4); }
+		inline bool operator != (Guid& rhs) { return *this == rhs; }
+
+	protected:
+		uint32 mU32[4];
+	};
+
+	namespace GuidTester
+	{
+		static void TestImportExport()
+		{
+			Guid generateGuid = Guid::Generate();
+			String str = generateGuid.ToString();
+			Guid guid1(str);
+			Guid guid2 = Guid::FromString(str);
+
+			DEBUG_CHECK(generateGuid == guid1);
+			DEBUG_CHECK(generateGuid == guid2);
+		}
+	}
+}
