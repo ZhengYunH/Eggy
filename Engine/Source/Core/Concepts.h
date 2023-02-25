c7b5be8ff7b72fe891684c870641fa610e72e534
ZhengYunH <631697250@qq.com>
2023-02-26 00:39:53 +0800

    more concept impl

--- Engine/Source/Core/Concepts.h
+++ Engine/Source/Core/Concepts.h
@@ -4,12 +4,14 @@
 // TODO: impl all concepts in https://www.enseignement.polytechnique.fr/informatique/INF478/docs/Cpp/en/cpp/concept.html
 namespace Eggy
 {
-	template<typename T>
-	concept cDefaultConstructible = std::is_default_constructible<T>::value;
+	template<typename _T>
+	concept cDefaultConstructible = std::is_default_constructible<_T>::value;
 
 	template<typename _Base, typename _Derived>
 	concept cDerivedFrom = std::derived_from<_Derived, _Base>;
 
+	template<typename _U, typename _V>
+	concept cConvertable = std::is_convertible_v<_U, _V>;
 
 	// Customize
 	template<typename T>
