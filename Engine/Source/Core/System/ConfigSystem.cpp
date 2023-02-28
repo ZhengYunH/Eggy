6d45855087a88eea08ce698251f72d7437fa0f92
运辉 郑 <631697250@qq.com>
2023-02-28 16:53:24 +0800

    config system implementation

--- Engine/Source/Core/System/ConfigSystem.cpp
+++ Engine/Source/Core/System/ConfigSystem.cpp
@@ -4,8 +4,29 @@ namespace Eggy
 {
 	DefineSystem(ConfigSystem);
 
+	Configuration* ConfigSystem::InsertConfiguration(const Name& config, bool bReplaceMode)
+	{
+		auto pair = mConfigs_.find(config);
+		if (pair != mConfigs_.end())
+		{
+			if (!bReplaceMode)
+				return pair->second;
+			else
+				pair->second->Clear();
+		}
+		else
+		{
+			mConfigs_[config] = new Configuration();
+		}
+		return mConfigs_[config];
+	}
+
 	void ConfigSystem::Initialize()
 	{
+		InsertConfiguration(GLOBAL_CONFIG.data());
+		SetGlobalConfig("AppWidth", 300);
+		SetGlobalConfig("AppHeight", 600);
+		SetGlobalConfig("IsFullScreen", false);
 	}
 
 	void ConfigSystem::Finalize()
--- Engine/Source/Core/System/ConfigSystem.h
+++ Engine/Source/Core/System/ConfigSystem.h
@@ -12,46 +12,90 @@ namespace Eggy
 		{
 		}
 
+		~ConfigItem()
+		{
+			Type->Destruct(Instance);
+			Instance = nullptr;
+		}
 		const Type* Type;
 		void* Instance;
 	};
 
 	class Configuration
 	{
-
+	public:
+		Configuration() = default;
+		void Clear()
+		{
+			for (auto& pair : mItems_)
+				delete pair.second;
+		}
+		~Configuration()
+		{
+			Clear();
+		}
+		Map<Name, ConfigItem*> mItems_;
 	};
 
 	class ConfigSystem : public TSystem<ConfigSystem>
 	{
 		DeclareSystem(ConfigSystem);
+
+		static constexpr std::string_view GLOBAL_CONFIG = "__Global";
 	public:
-		template<typename _TType>
-		_TType GetData(const Name& config, const Name& key);
+		Configuration* InsertConfiguration(const Name& config, bool bReplaceMode = false);
 
 		template<typename _TType>
-		_TType GetGlobalConfig(const Name& key, _TType defaultValue)
+		_TType GetData(const Name& config, const Name& key, const _TType defaultValue)
 		{
-			auto pairItr = mGlobalConfig_.find(key);
-			if (pairItr == mGlobalConfig_.end())
+			auto pair = mConfigs_.find(config);
+			if (pair == mConfigs_.end())
 				return defaultValue;
-			_TType ret = *((_TType*)pairItr->second->Instance);
-			return ret;
+			auto configuration = pair->second;
+			auto pairItr = configuration->mItems_.find(key);
+			if (pairItr == configuration->mItems_.end())
+				return defaultValue;
+			return *((_TType*)pairItr->second->Instance);
+		}
+		
+		template<typename _TType>
+		bool SetData(const Name& config, const Name& key, const _TType value)
+		{
+			auto pair = mConfigs_.find(config);
+			if (pair == mConfigs_.end())
+				return false;
+			auto configuration = pair->second;;
+			const Type* Type = GetType<_TType>();
+			_TType* rvalue = static_cast<_TType*>(Type->Construct());
+			*rvalue = value;
+			configuration->mItems_[key] = new ConfigItem(Type, (void*)rvalue);
+			return true;
 		}
 
 		template<typename _TType>
-		void SetGlobalConfig(const Name& key, _TType value)
+		const _TType GetGlobalConfig(const Name& key, const _TType defaultValue)
 		{
-			const Type* Type = Type::GetDefaultObject(); //GetType<_TType>();
-			_TType* rvalue = new _TType();
-			*rvalue = value;
-			mGlobalConfig_[key] = new ConfigItem(Type, (void*)rvalue);
+			return GetData<_TType>(GLOBAL_CONFIG.data(), key, defaultValue);
+		}
+
+		template<typename _TType>
+		bool SetGlobalConfig(const Name& key, const _TType value)
+		{
+			return SetData<_TType>(GLOBAL_CONFIG.data(), key, value);
 		}
 
 		void Initialize() override;
 		void Finalize() override;
 
 	protected:
-		List<Configuration*> mConfigs_;
-		Map<Name, ConfigItem*> mGlobalConfig_;
+		Configuration* GetConfiguration(const Name& key)
+		{
+			if (auto itr = mConfigs_.find(key); itr == mConfigs_.end())
+				return itr->second;
+			return nullptr;
+		}
+
+	protected:
+		Map<Name, Configuration*> mConfigs_;
 	};
 }
