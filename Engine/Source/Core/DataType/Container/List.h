ed4c3eabe21670614ba760310b0604474331c6ea
ZhengYunH <631697250@qq.com>
2023-07-19 23:56:16 +0800

    add more type

--- Engine/Source/Core/DataType/Container/List.h
+++ Engine/Source/Core/DataType/Container/List.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <vector>
+#include <list>
+#include <queue>
+
 
 namespace Eggy
 {
 	template<typename _Type>
 	using List = std::vector<_Type>;
+
+	template<typename _Type>
+	using LinkList = std::list<_Type>;
+
+	template<typename _Type>
+	using Queue = std::queue<_Type>;
 }
